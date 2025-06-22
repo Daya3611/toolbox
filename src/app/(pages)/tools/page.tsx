6d3314a46c465f0f82fa9e4000c8090bfbce3abc
Daya3611 <dayanand.gawade@vsit.edu.in>
2025-06-22 13:18:02 +0530

    bug fixed - reported by user Krishal hariya

--- src/app/(pages)/tools/page.tsx
+++ src/app/(pages)/tools/page.tsx
@@ -162,7 +162,7 @@ function ToolsPage() {
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
       {/* Background effects */}
       <div className="absolute inset-0">
-        <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-blue-500/10 rounded-full blur-3xl animate-pulse"></div>
+        {/* <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-blue-500/10 rounded-full blur-3xl animate-pulse"></div> */}
         <div className="absolute bottom-1/4 right-1/4 w-80 h-80 bg-purple-500/10 rounded-full blur-3xl animate-pulse delay-1000"></div>
       </div>
 
--- src/components/BugReportSection.tsx
+++ src/components/BugReportSection.tsx
@@ -271,7 +271,7 @@ Sent from ToolBox Bug Reporter`
     <div className="relative min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 pt-[100px]">
       {/* Animated background elements */}
       <div className="absolute inset-0">
-        <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-red-500/20 rounded-full blur-3xl animate-pulse"></div>
+        {/* <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-red-500/20 rounded-full blur-3xl animate-pulse"></div> */}
         <div className="absolute bottom-1/4 right-1/4 w-80 h-80 bg-orange-500/20 rounded-full blur-3xl animate-pulse delay-1000"></div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-64 h-64 bg-yellow-500/15 rounded-full blur-2xl animate-pulse delay-500"></div>
       </div>
